5d08f5230dc70a9ccca40d54e06afc9a8005fb63
Thomas Kapoulas <thomas.kapoulas@gmail.com>
2014-01-07 22:42:13 +0200

    le revhost

---
+++ revhost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2.7
+
+# requirements: python2.7, python-ipcalc
+
+import ipcalc
+import socket
+import argparse
+
+a_desc = '''Reverse hostname scanner - revhost.py'''
+
+parser = argparse.ArgumentParser(description=a_desc,formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('target', help='IPv4/IPv6 address or CIDR')
+parser.add_argument('-v','--verbose', help='enables verbosity', action='store_true')
+args = parser.parse_args()
+
+try:
+	for ipaddr in ipcalc.Network(args.target):
+		try:
+		    attr = socket.gethostbyaddr(str(ipaddr))
+		    print "%s resolves to: \033[92m%s\033[0m" % (ipaddr, attr[0])
+		except socket.herror as e:
+                        if args.verbose:
+			    print "%s \033[91m(unknown host)\033[0m" % (ipaddr)
+except KeyboardInterrupt:
+	print "Scan terminated by user."
