64bedb50adef52f570a5a962dc2759382edf2b0e
Thomas Kapoulas <thomas.kapoulas@gmail.com>
2014-01-08 19:34:03 +0200

    fixed identation & added exception for ValueError

--- revhost.py
+++ revhost.py
@@ -4,22 +4,25 @@
 
 import ipcalc
 import socket
-import argparse
+import argparse as ap
 
 a_desc = '''Reverse hostname scanner - revhost.py'''
 
-parser = argparse.ArgumentParser(description=a_desc,formatter_class=argparse.RawDescriptionHelpFormatter)
+parser = ap.ArgumentParser(description=a_desc,formatter_class=ap.RawDescriptionHelpFormatter)
 parser.add_argument('target', help='IPv4/IPv6 address or CIDR')
-parser.add_argument('-v','--verbose', help='enables verbosity', action='store_true')
+parser.add_argument('-v', '--verbose', help='enables verbosity', action='store_true')
 args = parser.parse_args()
 
 try:
-	for ipaddr in ipcalc.Network(args.target):
-		try:
-		    attr = socket.gethostbyaddr(str(ipaddr))
-		    print "%s resolves to: \033[92m%s\033[0m" % (ipaddr, attr[0])
-		except socket.herror as e:
-                        if args.verbose:
-			    print "%s \033[91m(unknown host)\033[0m" % (ipaddr)
+    for ipaddr in ipcalc.Network(args.target):
+        try:
+            attr = socket.gethostbyaddr(str(ipaddr))
+            print "%s resolves to: \033[92m%s\033[0m" % (ipaddr, attr[0])
+        except socket.herror as e:
+            if args.verbose:
+                print "%s \033[91m(unknown host)\033[0m" % (ipaddr)
+except ValueError:
+    print "Wrong arguments. Please enter only IPv4/6 addresses and CIDR."
+
 except KeyboardInterrupt:
-	print "Scan terminated by user."
+    print "Scan terminated by user."
